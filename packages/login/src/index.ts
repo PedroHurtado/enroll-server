e52c77d3be3fb842b9899765e84a721479570f60
Pedro Hurtado <pedrohurt@gmail.com>
2025-02-04 19:30:31 +0100

    add pino to index.ts

--- packages/login/src/index.ts
+++ packages/login/src/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 import cors from "cors";
+import pino from 'pino';
+import pinoHttp from 'pino-http';
+import pinoElastic from 'pino-elasticsearch';
 import { registerFeatures } from '@enroll-server/common'
-
+const streamToElastic = pinoElastic({
+  index: 'logs-app',  
+  node: 'http://elasticsearch:9200'  // Cambia esto a tu URL de Elasticsearch
+});
+const logger = pino({}, streamToElastic);
+const httpLogger = pinoHttp({ logger });
 
 const corsOptions = {
   origin: "*",
@@ -12,6 +20,7 @@ const corsOptions = {
 const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(httpLogger);
 
 const PORT = process.env.PORT || 3000;
 
