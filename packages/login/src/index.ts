dfa211e78f0edf5476e447d5c2cc0fc63c38fa7a
Pedro Hurtado <pedrohurt@gmail.com>
2025-02-04 18:07:51 +0100

    add expres.json and cors to login service

--- packages/login/src/index.ts
+++ packages/login/src/index.ts
@@ -1,7 +1,20 @@
 import express from 'express';
+import cors from "cors";
 import { registerFeatures } from '@enroll-server/common'
+
+
+const corsOptions = {
+  origin: "*",
+  methods: ["GET","POST","PUT", "PATCH", "DELETE", "HEAD"],  
+  maxAge: 3600
+};
+
 const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
+
 async function init(){
   try{
     await registerFeatures(app)
