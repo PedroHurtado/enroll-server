3f0eeb25e17df75ad861abab2ba8ca47b83cccb8
Pedro Hurtado <pedrohurt@gmail.com>
2025-02-05 11:15:49 +0100

    run decorators

--- packages/common/src/decorators/index.ts
+++ packages/common/src/decorators/index.ts
@@ -2,11 +2,17 @@ import { Logger } from "pino";
 
 export function LoggerDecorator(logger: Logger) {
     return (target: any, context: ClassMethodDecoratorContext) => {
-        console.log(`DataBase Decorator applied on: ${String(context.name)}`);
+        const originalMethod = target;
+        return async function(this:any,...args: any[]) {
+            originalMethod.apply(this, args);
+        }
     };
 }
 export function DataBaseDecorator(connection: string) {
     return (target: any, context: ClassMethodDecoratorContext) => {
-        console.log(`DataBase Decorator applied on: ${String(context.name)}`);
+        const originalMethod = target;
+        return async function(this:any,...args: any[]) {
+            originalMethod.apply(this, args);
+        }
     };
 }
