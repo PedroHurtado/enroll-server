092e5890bca784b2fcfbfb7c96fc738e820b18ca
Pedro Hurtado <pedrohurt@gmail.com>
2025-02-06 11:45:55 +0100

    redis middelware

---
+++ packages/common/src/redismiddelware.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express';
+import { Redis } from 'ioredis';
+
+declare module "express" {
+    export interface Request {
+        tenantId?: string;
+    }
+}
+export function useRedis() {
+    const redis = new Redis("redis://localhost:6379");
+    const gracefulShutdown = async () => {
+        console.log("Cerrando conexión con Redis...");
+        await redis.quit();
+        console.log("Conexión con Redis cerrada.");
+        process.exit(0);
+    };
+    process.on("SIGINT", gracefulShutdown);
+    process.on("SIGTERM", gracefulShutdown);
+
+    return async function (req: Request, res: Response, next: NextFunction) {
+        const subdomain = req.hostname.split('.')[0];
+        if (!subdomain || subdomain === "example") {
+            return res.status(400).json({ error: "Subdominio inválido" });
+        }
+        const tenantId = await redis.get(`tenant:${subdomain}`);
+
+        if (!tenantId) {
+            return res.status(404).json({ error: "Tenant no encontrado" });
+        }
+        req.tenantId = tenantId;
+        next();
+    }
+}
