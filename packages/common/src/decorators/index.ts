a080eb3e8457ec28bf62aba77b007049a6a7733e
Pedro Hurtado <pedrohurt@gmail.com>
2025-02-06 13:24:33 +0100

    otp

--- packages/common/src/decorators/index.ts
+++ packages/common/src/decorators/index.ts
@@ -3,18 +3,18 @@ import { Logger } from "pino";
 export function Log(logger: Logger) {
     return (target: any, context: ClassMethodDecoratorContext) => {
         const originalMethod = target;
-        return async function(this:any,...args: any[]) {
+        return async function(this:any,...args: any[]):Promise<any> {
             console.log("Log decorator")
-            originalMethod.apply(this, args);
+            return originalMethod.apply(this, args);
         }
     };
 }
 export function Connection(connection: string) {
     return (target: any, context: ClassMethodDecoratorContext) => {
         const originalMethod = target;
-        return async function(this:any,...args: any[]) {
+        return async function(this:any,...args: any[]):Promise<any> {
             console.log("Connection decorator")
-            originalMethod.apply(this, args);
+            return originalMethod.apply(this, args);
         }
     };
 }
