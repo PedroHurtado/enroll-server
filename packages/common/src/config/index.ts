c16c6ba644d12ed4e53cace356ad0c58b5240f55
Pedro Hurtado <pedrohurt@gmail.com>
2025-02-11 12:52:02 +0100

    add Object.freeze and rollup export all objects

--- packages/common/src/config/index.ts
+++ packages/common/src/config/index.ts
@@ -1,8 +1,12 @@
-import { dev } from './dev';
-import { prod } from './prod';
+
+import { dev } from "./dev";
+import { prod } from "./prod";
 
 const ENV = process.argv.includes("--prod") ? "prod" : "dev";
-const config = ENV === "prod" ? prod : dev;
+const config = Object.freeze(ENV === "prod" ? prod : dev);
 
-export {config}
+export { config };
+
+
+
 
