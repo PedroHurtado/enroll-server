bbc36d2e24a5cfe95bee149d8bb852cf044b8126
Pedro Hurtado <pedrohurt@gmail.com>
2025-02-11 14:45:37 +0100

    change elastic by elasticsearch en prod.dev

--- packages/common/src/config/prod.ts
+++ packages/common/src/config/prod.ts
@@ -1,7 +1,7 @@
 export const prod={
     mongo:'mongodb://root:example@mongo:27017/nueva_bd?authSource=admin',
     redis:'redis://redis:6379',
-    elastic:'http://elastic:9200',
+    elastic:'http://elasticsearch:9200',
     kong:'http://kong:8001'
 }
 
