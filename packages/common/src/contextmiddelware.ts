954c807c444b6744b9028319b60dde08eb674018
Pedro Hurtado <pedrohurt@gmail.com>
2025-02-13 15:37:18 +0100

    change context middelware

--- packages/common/src/contextmiddelware.ts
+++ packages/common/src/contextmiddelware.ts
@@ -1,14 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import { requestContext } from "./requestcontext";
 export function context() {
-    return async function (req: Request, res: Response, next: NextFunction) {
+    return  function (req: Request, res: Response, next: NextFunction) {
 
         const headers = {
             'Authorization': req.headers['authorization'] as string || '',
             'x-tenant': req.headers['x-tenant'] as string || '',
         };
-        requestContext.run(headers, ()=>{
-            next()
-        })
+        return requestContext.run(headers, next)
     }
 } 
