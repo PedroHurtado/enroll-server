9e82614de94030de1ff9afb38226968034ba75d6
Pedro Hurtado <pedrohurt@gmail.com>
2025-03-05 11:32:17 +0100

    add getkeys in common

---
+++ packages/common/src/getkeys.ts
@@ -0,0 +1,26 @@
+import fs from 'fs'
+import path from 'path';
+const secretsDir = '/run/secrets';
+const privateKeyPath = path.join(secretsDir, 'private.pem');
+const publicKeyPath = path.join(secretsDir, 'public.pem');
+
+let privateKey: string = ''
+let publicKey: string = ''
+
+export interface Keys {
+    privateKey: string
+    publicKey: string
+}
+
+export const getKeys = (): Keys => {
+    if (!privateKey) {
+        privateKey = fs.readFileSync(privateKeyPath, "utf-8")
+    }
+    if (!publicKey) {
+        publicKey = fs.readFileSync(publicKeyPath, "utf-8")
+    }
+    return {
+        privateKey,
+        publicKey
+    }
+}
